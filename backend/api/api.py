b381c6f2cdb113766d422e5f03e74b52f0e7d835
NT1026 <nelson20020222@gmail.com>
2025-03-17 02:10:45 +0800

    feat: Frontend login template first part

--- backend/api/api.py
+++ backend/api/api.py
@@ -16,13 +16,9 @@ app.include_router(auth_router)
 app.include_router(info_router)
 app.include_router(user_router)
 
-
-origins = [
-    "http://localhost:3000",
-]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
